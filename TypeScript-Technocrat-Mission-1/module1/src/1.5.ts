c20b43a5dca507597c117d91d8e65709770e84fc
codewithsaidul <codewithsaidul@gmail.com>
2025-05-01 14:07:54 +0600

    Learning object, optional, literal types in ts

---
+++ TypeScript-Technocrat-Mission-1/module1/src/1.5.ts
@@ -0,0 +1,21 @@
+
+
+// object, optional data types
+
+
+const person: {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    age: number;
+    country: string;
+    isSingle: boolean;
+    company: "Programming Hero"
+} = {
+    firstName: "Saidul",
+    lastName: "Islam Rana",
+    age: 23,
+    country: "Bangladesh",
+    isSingle: true,
+    company: "Programming Hero"
+}
