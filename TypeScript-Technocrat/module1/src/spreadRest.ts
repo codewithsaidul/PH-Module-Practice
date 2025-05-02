c8674151b387770ac6e2d325eae7fa3fbe5ef811
codewithsaidul <codewithsaidul@gmail.com>
2025-05-03 00:50:41 +0600

    Learn Spread & Rest Operator

---
+++ TypeScript-Technocrat/module1/src/spreadRest.ts
@@ -0,0 +1,39 @@
+{
+  // run command ts-node ./module1/src/spreadRest.ts
+  // Learn Spread & Rest Operator
+
+  const friend1: string[] = ["mitu", "sadia"];
+  const friend2: string[] = ["noman", "jobayer"];
+
+  // spreading friend1 & friend2 array
+  const friends = [...friend1, ...friend2];
+  // console.log(friend)
+
+  const mentors1 = {
+    typescript: "Mezba Bhai",
+    Redux: "Mir Bhai",
+    Next: "Mizan Bhai",
+  };
+
+  const mentors2 = {
+    prisma: "Tonmoy Bhai",
+    Mongoose: "Rizvi Bhai",
+    GraphQL: "Rakib Bhai",
+  };
+
+  // spreading two objects into one common objects
+  const mentors = {
+    ...mentors1,
+    ...mentors2,
+  };
+
+  // console.log(mentors)
+
+  // =================== rest operator
+
+  function restOp(...friends: string[]) {
+    return friends.map((friend: string) => `Hi ${friend}`);
+  }
+
+  console.log(restOp("numan", "kulsuma", "sangita", "jubayer"));
+}
