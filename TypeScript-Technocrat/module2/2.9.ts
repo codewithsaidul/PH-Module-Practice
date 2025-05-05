f35ca6e9c9c3543a41467115d45199d4a0f46cff
codewithsaidul <codewithsaidul@gmail.com>
2025-05-05 13:34:37 +0600

    Learning Conditional Types

---
+++ TypeScript-Technocrat/module2/2.9.ts
@@ -0,0 +1,23 @@
+{
+    // conditional types in typescript
+
+    type a = number;
+    type b = string;
+
+    // conditionaly checking 
+    type c = a extends null ? "Its Null" : "Its not null";
+
+    // nested type checking
+    type d = a extends null ? "yeah im null" : b extends undefined ? "yeah im undefined" : "Im not null or undefined"
+
+    type Sheikh = {
+        bike: string;
+        car: string;
+        helicopter: string
+    }
+
+
+    type checkVechicle<T> = T extends keyof Sheikh ? true : false
+
+    type hasBike = checkVechicle<"train">
+}
