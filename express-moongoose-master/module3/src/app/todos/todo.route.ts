e0f39f22cc47ea7ab060e4ba2159e6c0cf8d114c
codewithsaidul <codewithsaidul@gmail.com>
2025-06-01 18:41:08 +0600

    mongodb connected with todo-express app

--- express-moongoose-master/module3/src/app/todos/todo.route.ts
+++ express-moongoose-master/module3/src/app/todos/todo.route.ts
@@ -7,6 +7,22 @@ export const todosRouter = express.Router();
 const filePath = path.join(__dirname, "../../../db/todos.json");
 const data = fs.readFileSync(filePath, { encoding: "utf-8" });
 
+
+// get all todos
 todosRouter.get("/", (req: Request, res: Response) => {
   res.send(data);
+});
+
+
+
+
+todosRouter.post("/createTodo", (req: Request, res: Response) => {
+  const { title, body } = req.body;
+
+  res.send({
+    message: "Create a new todo",
+    data: {
+        title, body
+    }
+  });
 });
--- express-moongoose-master/module3/src/server.ts
+++ express-moongoose-master/module3/src/server.ts
@@ -1,14 +1,29 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
 import app from "./app";
 
 const port = 3000;
 let server;
 
+const uri =
+  "mongodb+srv://newsAdmin:Lr7GNjEru2Ffub4G@cluster0.lggjuua.mongodb.net/todoDB?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 const bootstrap = async () => {
-  app.listen(port, () => {
+  await client.connect();
+  console.log("connected to mongodb");
+  const db =  await client.db("todoDB");
+  const collection = await db.collection("todos").insertOne({ title: "MongoDB", body: "Learning MongoDB"})
+  console.log(collection)
+  server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
 };
-
-
 
 bootstrap();
