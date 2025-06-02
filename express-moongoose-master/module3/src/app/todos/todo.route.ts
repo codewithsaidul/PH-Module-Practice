6913811700ff5ec6931132a8120807b488f03991
codewithsaidul <codewithsaidul@gmail.com>
2025-06-02 14:04:05 +0600

    get single todo data

--- express-moongoose-master/module3/src/app/todos/todo.route.ts
+++ express-moongoose-master/module3/src/app/todos/todo.route.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { client } from "../../config/mongodb";
+import { ObjectId } from "mongodb";
 
 export const todosRouter = express.Router();
  const db = client.db("todoDB");
@@ -16,6 +17,26 @@ todosRouter.get("/", async (req: Request, res: Response) => {
   res.json(todos);
 });
 
+
+// get a single todo by id
+todosRouter.get("/:id", async (req: Request, res: Response) => {
+  const todoId = req.params.id
+
+  // console.log(todoId)
+
+  const query = { _id: new ObjectId(todoId)}
+
+  const todos = await collection.findOne(query);
+
+// console.log(todos)
+
+  // const todos = await cursor.toArray();
+
+  res.send(todos);
+});
+
+
+// create a new todo
 todosRouter.post("/createTodo", async (req: Request, res: Response) => {
   const { title, description, priority } = req.body;
   await collection.insertOne({
