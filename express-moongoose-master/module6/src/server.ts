46438d104c6ecb4b09913f2aea562459fad355da
codewithsaidul <codewithsaidul@gmail.com>
2025-06-15 01:59:42 +0600

    starting learning mongoose

---
+++ express-moongoose-master/module6/src/server.ts
@@ -0,0 +1,22 @@
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+require('dotenv').config();
+
+const port = 3000;
+let server: Server;
+
+
+async function main() {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lggjuua.mongodb.net/todoDB?retryWrites=true&w=majority&appName=Cluster0`);
+    server = app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+main()
