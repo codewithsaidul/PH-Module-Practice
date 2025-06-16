d62178ed75615a16d62b9c3baf489ecdb251fa67
codewithsaidul <codewithsaidul@gmail.com>
2025-06-16 21:59:23 +0600

    learning mongoose validation error

--- express-moongoose-master/module6/src/app/models/user.models.ts
+++ express-moongoose-master/module6/src/app/models/user.models.ts
@@ -1,28 +1,42 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interface/user.interface";
 
-const userSchema = new Schema<IUser>({
-  firstName: { type: String, required: true, trim: true },
-  lastName: { type: String, required: true, trim: true },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "First Name must be minimum 3 character"],
+      maxlength: [50, "First Name must be maximum 50 character"],
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "Last Name must be minimum 3 character"],
+      maxlength: [50, "Last Name must be maximum 50 character"],
+    },
+    age: { type: Number, required: true, min: [18, 'Age must be getter then 18, but got {VALUE}'], max: [32, 'Age must be less then 32, but got {VALUE}'] },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      uppercase: true,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
   },
-  password: { type: String, required: true },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-}, 
-{
+  {
     versionKey: false,
-    timestamps: true
-}
+    timestamps: true,
+  }
 );
 
-
-
-
-export const User = model("User", userSchema)
+export const User = model("User", userSchema);
