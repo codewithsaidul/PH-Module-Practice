a84707e4e77a6ca87243b055c7797ba7f836a615
codewithsaidul <codewithsaidul@gmail.com>
2025-06-18 14:03:40 +0600

    custom validation added on email and enum

--- express-moongoose-master/module6/src/app/models/user.models.ts
+++ express-moongoose-master/module6/src/app/models/user.models.ts
@@ -1,5 +1,6 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interface/user.interface";
+import validator from "validator"
 
 const userSchema = new Schema<IUser>(
   {
@@ -17,19 +18,34 @@ const userSchema = new Schema<IUser>(
       minlength: [3, "Last Name must be minimum 3 character"],
       maxlength: [50, "Last Name must be maximum 50 character"],
     },
-    age: { type: Number, required: true, min: [18, 'Age must be getter then 18, but got {VALUE}'], max: [32, 'Age must be less then 32, but got {VALUE}'] },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, "Age must be getter then 18, but got {VALUE}"],
+      max: [32, "Age must be less then 32, but got {VALUE}"],
+    },
     email: {
       type: String,
       lowercase: true,
-      unique: true,
+      unique: [true, "Email already exists!"],
       required: true,
       trim: true,
+      // validate: {
+      //   validator: function (v) {
+      //     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v)
+      //   },
+      //   message: props => `${props.value} is not a valid email`
+      //  }
+      validate: [validator.isEmail, "{VALUE} isnt a valided email"],
     },
     password: { type: String, required: true },
     role: {
       type: String,
       uppercase: true,
-      enum: ["USER", "ADMIN"],
+      enum: {
+        values: ["USER", "ADMIN"],
+        message: "Provided rule is not valid.",
+      },
       default: "USER",
     },
   },
